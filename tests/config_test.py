fe13f59fc2ac52f382502620da00f5d77c6525fe
Quentin Ferré <quentin.q.ferre@gmail.com>
2024-09-02 21:41:06 +0200

    Issues

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,3 +1,1 @@
-# TODO Set up Continuous Integration in GitHub, to run all these tests each time
-
 # TODO assert in config that each hexagon_type defined has a corresponding asset in assets/hexes. Same fot the unit types and assets/units.
